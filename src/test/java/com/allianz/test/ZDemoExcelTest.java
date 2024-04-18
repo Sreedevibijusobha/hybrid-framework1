ddd2daa03978ec69582827dac50bdbf6150a8288
Sreedevi <sreedevibiju5@gmail.com>
2024-04-18 13:37:05 +0530

    msg4

---
+++ src/test/java/com/allianz/test/ZDemoExcelTest.java
@@ -0,0 +1,15 @@
+package com.allianz.test;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+public class ZDemoExcelTest {
+	
+	public static void main(String[] args) throws FileNotFoundException
+	{
+		FileInputStream file = new FileInputStream("src/test/resources/test_data/hrm_data.xlsx");
+	//	XSSFWorkbook book = new XSSFWorkbook(file);
+	//	XSSFSheet sheet
+	}
+
+}
